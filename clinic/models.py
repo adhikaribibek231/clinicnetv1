a34ed0a35c1d17b6776b7b10c7cc5d93744f3080
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-05-27 17:36:07 +0545

    models update

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    mobile = models.Int
+    special = models.CharField(max_length=50)
+
+    def __str__(self):
+        
