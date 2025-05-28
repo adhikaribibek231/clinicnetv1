2d2a429d6006c0d1e30a4f51586cd4f303927279
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-05-28 13:13:54 +0545

    views.py updated with login and logout function

--- clinic/views.py
+++ clinic/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import users
+from django.contrib.auth import authenticate, logout, login
 # Create your views here.
 def About(request):
     return render(request, 'about.html')
 def Index(request):
     if not request.user.is_staff:
         return redirect('login')
-    return render(request, 'about.html')
+    return render(request, 'index.html')
+def Login(request):
+    error=""
+    if request.method=='POST':
+        u= request.POST['uname']
+        p= request.POST['pwd']
+        user = authenticate(username=u, password=p)
+        try:
+            if not request.user.is_staff:
+                login(request,user)
+                error="no"
+            else:
+                error="yes"
+        except:
+            error="yes"
+    d={'error':error}
+    return render(request, 'index.html',d)
+
+def Logout_admin(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    logout(request)
+    return redirect('login')
