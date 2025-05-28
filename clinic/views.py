48373a104c12d4b4cf6241cb133bc644a0d201d7
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-05-28 13:02:18 +0545

    index.html updated, and views updated

--- clinic/views.py
+++ clinic/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import users
 # Create your views here.
 def About(request):
     return render(request, 'about.html')
-    return render()
+def Index(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    return render(request, 'about.html')
